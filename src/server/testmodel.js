131a9736092897f7563f7b636c768026cf62fd91
Adrian Bryant <127413341+Bles-t@users.noreply.github.com>
2025-01-11 16:50:38 -0600

    bug fix

--- src/server/testmodel.js
+++ src/server/testmodel.js
@@ -1,18 +1,19 @@
-const { generateEventDescription } = require('../utils/llm');
-require("dotenv").config();
+// const { generateEventDescription } = require('../utils/llm');
+// require("dotenv").config();
 
-(async () => {
-    const testEvent = {
-        title: "The Rise of Hip-Hop",
-        date: "1973-08-11",
-        artist: "DJ Kool Herc",
-        description: "A groundbreaking party in the Bronx.",
-    };
+// (async () => {
+//     const testEvent = {
+//         title: "The Rise of Hip-Hop",
+//         date: "1973-08-11",
+//         artist: "DJ Kool Herc",
+//         description: "A groundbreaking party in the Bronx.",
+//     };
 
-    try {
-        const description = await generateEventDescription(testEvent);
-        console.log("Generated Description:", description);
-    } catch (error) {
-        console.error("Error:", error.message);
-    }
-})();
+//     try {
+//         const description = await generateEventDescription(testEvent);
+//         console.log("Generated Description:", description);
+//     } catch (error) {
+//         console.error("Error:", error.message);
+//     }
+// })();
+// no test written 
