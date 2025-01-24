d0a06f909d4a1cd4655a9a682ca56290985ec9c9
Adrian Bryant <127413341+Bles-t@users.noreply.github.com>
2025-01-23 20:18:02 -0600

    Render cors fix

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5002', // Allow your React app's origin
+    origin: ['http://localhost:5002', 'https://hiphoptimeline.vercel.app'], // Allow your React app's origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
     credentials: true // Enable cookies and sessions to be sent
 }));
